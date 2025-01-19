cb3c8e14ef1e38fd0e45f94335e315362f68e807
rucci36 <ruka36lil18@gmail.com>
2025-01-20 01:20:07 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        //　値の代入
+        $food_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+        // 連想配列を出力
+        print_r($food_data);
+        ?>
+        </p>
+</body>
+
+</html>
